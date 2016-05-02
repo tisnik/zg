90c06aa01cc88506be611cdc1bf609a6f6cc7cce
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-05-02 12:45:00 +0200

    Updated project file - added dependencies and Clojure ring handler

--- project.clj
+++ project.clj
@@ -1,9 +1,21 @@
 (defproject zg "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main ^:skip-aot zg.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+    :description "FIXME: write description"
+    :url "http://example.com/FIXME"
+    :license {:name "Eclipse Public License"
+              :url "http://www.eclipse.org/legal/epl-v10.html"}
+    :dependencies [[org.clojure/clojure "1.6.0"]
+                   [org.clojure/java.jdbc "0.3.5"]
+                   [org.clojure/tools.cli "0.3.1"]
+                   [org.xerial/sqlite-jdbc "3.7.2"]
+                   [ring/ring-core "1.3.2"]
+                   [ring/ring-jetty-adapter "1.3.2"]
+                   [hiccup "1.0.4"]]
+    :dev-dependencies [[lein-ring "0.8.10"]]
+    :plugins [[lein-ring "0.8.10"]
+              [codox "0.8.11"]
+              [test2junit "1.1.0"]
+              [lein-cloverage "1.0.6"]]
+    :ring {:handler zg.core/app}
+    :main ^:skip-aot zg.core
+    :target-path "target/%s"
+    :profiles {:uberjar {:aot :all}})
