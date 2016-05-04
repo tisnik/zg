6a21e6b5b672362a1b6ee6fb41c3be1279af99b0
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-05-04 14:50:19 +0200

    Stub for config_test.clj

---
+++ test/zg/config_test.clj
@@ -0,0 +1,13 @@
+(ns zg.config-test
+  (:require [clojure.test :refer :all]
+            [zg.config :refer :all]))
+
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
