1288ee07ba3a05bde9cc5dd0ec1e6c93bf3de0cd
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-05-09 14:02:53 +0200

    Three tests added into core_test.clj

--- test/zg/core_test.clj
+++ test/zg/core_test.clj
@@ -23,3 +23,27 @@
     [function-name]
     (clojure.test/function? function-name))
 
+;
+; Tests for various functions
+;
+
+(deftest test-start-server-existence
+    "Check that the zg.core/start-server definition exists."
+    (testing "if the zg.core/start-server definition exists."
+        (is (callable? 'zg.core/start-server))))
+
+(deftest test-get-and-check-port-existence
+    "Check that the zg.core/get-and-check-port definition exists."
+    (testing "if the zg.core/get-and-check-port definition exists."
+        (is (callable? 'zg.core/get-and-check-port))))
+
+(deftest test-get-port-existence
+    "Check that the zg.core/get-port definition exists."
+    (testing "if the zg.core/get-port definition exists."
+        (is (callable? 'zg.core/get-port))))
+
+(deftest test--main-existence
+    "Check that the zg.core/-main definition exists."
+    (testing "if the zg.core/-main definition exists."
+        (is (callable? 'zg.core/-main))))
+
