e09c5f4a90782c64df570ccbbc5c6090874f683c
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-05-12 16:02:47 +0200

    Updated DB schema - dictionary type

--- database/create_database.sql
+++ database/create_database.sql
@@ -1,5 +1,6 @@
 create table dictionary (
     word        text primary key,
+    dictionary  char, -- 'w':whitelist  'b': blacklist
     user        text,
     datetime    text,
     deleted     integer,
