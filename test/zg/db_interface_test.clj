7727d2ba8e5a816685ddf33b30aa38e215a08c49
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-05-17 12:32:05 +0200

    Four tests for db-interface module: status->int, dictionary-type->char, insert-word-into-dictionary, and add-new-word-into-dictionary

--- test/zg/db_interface_test.clj
+++ test/zg/db_interface_test.clj
@@ -23,3 +23,31 @@
     [function-name]
     (clojure.test/function? function-name))
 
+;
+; Tests for function existence
+;
+
+(deftest test-status->int-existence
+    "Check that the zg.db-interface/status->int definition exists."
+    (testing "if the zg.db-interface/status->int definition exists."
+        (is (callable? 'zg.db-interface/status->int))))
+
+
+(deftest test-dictionary-type->char-existence
+    "Check that the zg.db-interface/dictionary-type->char definition exists."
+    (testing "if the zg.db-interface/dictionary-type->char definition exists."
+        (is (callable? 'zg.db-interface/dictionary-type->char))))
+
+
+(deftest test-insert-word-into-dictionary-existence
+    "Check that the zg.db-interface/insert-word-into-dictionary definition exists."
+    (testing "if the zg.db-interface/insert-word-into-dictionary definition exists."
+        (is (callable? 'zg.db-interface/insert-word-into-dictionary))))
+
+
+(deftest test-add-new-word-into-dictionary-existence
+    "Check that the zg.db-interface/add-new-word-into-dictionary definition exists."
+    (testing "if the zg.db-interface/add-new-word-into-dictionary definition exists."
+        (is (callable? 'zg.db-interface/add-new-word-into-dictionary))))
+
+
