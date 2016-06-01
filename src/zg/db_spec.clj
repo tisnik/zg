e51a4a899596de001b9d244d76eb72c154e7f9ae
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-06-01 11:08:17 +0200

    Added comments to all defs in db-spec module

--- src/zg/db_spec.clj
+++ src/zg/db_spec.clj
@@ -7,13 +7,14 @@
 ;  http://www.eclipse.org/legal/epl-v10.html
 ;
 ;  Contributors:
-;      Pavel Tisnovsky
+;      Pavel Tisnovsky
 ;
 
 (ns zg.db-spec
     "Namespace that contains configuration of all JDBC sources.")
 
 (def zg-db
+    "Specification for SQLite database used for storing all dictionaries."
     {:classname   "org.sqlite.JDBC"
      :subprotocol "sqlite"
      :subname     "zg.db"
