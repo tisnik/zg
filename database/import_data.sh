ae7a78305fe9329828071f4ff27ac1ed3af26b8a
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-11 15:24:46 +0200

    Script to import database (both dictionaries)

---
+++ database/import_data.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#
+#  (C) Copyright 2016  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+DATABASE=zg.db
+
+cat database_dump_1.sql | sqlite3 ../${DATABASE}
+cat database_dump_2.sql | sqlite3 ../${DATABASE}
+
