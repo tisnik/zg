73b7dcf2ee37ec3470569d5151cd8c35e790d0b7
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-10-25 16:19:20 +0200

    Bump up Clojure version + fixed URL to the 'zg' repository

--- project.clj
+++ project.clj
@@ -7,15 +7,15 @@
 ;  http://www.eclipse.org/legal/epl-v10.html
 ;
 ;  Contributors:
-;      Pavel Tisnovsky
+;      Pavel Tisnovsky
 ;
 
 (defproject zg "0.1.0-SNAPSHOT"
     :description "FIXME: write description"
-    :url "http://example.com/FIXME"
+    :url "http://github.com/tisnik/zg"
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.6.0"]
+    :dependencies [[org.clojure/clojure "1.7.0"]
                    [org.clojure/java.jdbc "0.3.5"]
                    [org.clojure/tools.cli "0.3.1"]
                    [org.clojure/data.csv  "0.1.2"]
