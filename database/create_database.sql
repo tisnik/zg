adbd5c5b00e277e51724879935c0f257679fddf4
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-12-23 13:31:17 +0100

    Updated DB schema - new column added + composed key should be used

--- database/create_database.sql
+++ database/create_database.sql
@@ -1,10 +1,11 @@
 create table dictionary (
-    word        text primary key,
+    word        text,
     dictionary  char, -- 'w':whitelist  'b': blacklist   'a':atomic typos
     user        text,
     datetime    text,
     deleted     integer,
     description text,
-    correct     text  -- used by atomic typos only
+    correct     text, -- used by atomic typos only
+    primary key (word, dictionary)
 );
 
