2b906a5c681c5de2bd08fc1f86cf88c7b3bf276f
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-01-24 16:01:19 +0100

    DB script to import (test) sources into the 'sources' table

---
+++ database/sources.sql
@@ -0,0 +1,6 @@
+BEGIN TRANSACTION;
+insert into "sources" (id, source) values(0, 'source1');
+insert into "sources" (id, source) values(1, 'source2');
+insert into "sources" (id, source) values(2, 'source3');
+COMMIT;
+
