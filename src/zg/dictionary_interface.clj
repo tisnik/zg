a852f6debf043e922183897a0702e7a8f7955544
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-04-11 12:34:08 +0200

    Proper processing of word classes and 'use it' drop down list

--- src/zg/dictionary_interface.clj
+++ src/zg/dictionary_interface.clj
@@ -25,6 +25,31 @@
     [word]
     (re-matches #"[A-Za-z0-9.'\-\s_]+" word))
 
+(defn process-use-it
+    [use-it]
+    (condp = use-it
+        "Yes" 1
+        "No"  0
+              2))
+
+(defn process-class
+    [class]
+    (condp = (.toLowerCase class)
+        "N/A" nil
+        "n/a" nil
+        "verb" 1
+        "noun" 2
+        "adverb" 3
+        "adjective" 4
+        "pronoun" 5
+        "conjunction"  6
+        "preposition"  7
+        "interjection" 8
+        "article"      9
+        "numeral"      10
+        "determiner"  11
+        "exclamation" 12))
+
 (defn store-word
     "Store one word into the dictionary."
     ([word correct-form description user-name dictionary-type]
@@ -33,7 +58,7 @@
     (db-interface/add-new-word-into-dictionary word correct-form description (or user-name "(*unknown*)") dictionary-type))
     ([word description class use-it internal copyright source correct-forms incorrect-forms user-name dictionary-type]
     (log/info "About to store following word:" word " with description: " description " into dictionary: " (str dictionary-type))
-    (db-interface/add-new-word-into-dictionary word description class use-it internal copyright source correct-forms incorrect-forms (or user-name "(*unknown*)") dictionary-type)))
+    (db-interface/add-new-word-into-dictionary word description (process-class class) (process-use-it use-it) internal copyright (inc source) correct-forms incorrect-forms (or user-name "(*unknown*)") dictionary-type)))
 
 (defn store-words
     "Store sequence of words into the dictionary."
