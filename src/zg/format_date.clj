df1fecda30412974ffb8a88cf3b4efa930ebcb15
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-16 10:51:03 +0200

    Reformatted format_date.clj by zprint

--- src/zg/format_date.clj
+++ src/zg/format_date.clj
@@ -11,20 +11,20 @@
 ;
 
 (ns zg.format-date
-    "Functions for working with dates and datetimes.")
+  "Functions for working with dates and datetimes.")
 
 (def unified-date-format
-    "Object with the specification how date should be formatted."
-    (new java.text.SimpleDateFormat "yyyy-MM-dd HH:mm:ss"))
+  "Object with the specification how date should be formatted."
+  (new java.text.SimpleDateFormat "yyyy-MM-dd HH:mm:ss"))
 
 (defn format-date
-    "Format given date using unified date format."
-    [^java.util.Date date]
-    (let [^java.text.DateFormat format-object unified-date-format]
-        (.format format-object date)))
+  "Format given date using unified date format."
+  [^java.util.Date date]
+  (let [^java.text.DateFormat format-object unified-date-format]
+    (.format format-object date)))
 
 (defn format-current-date
-    "Format current date using unified date format."
-    []
-    (format-date (new java.util.Date)))
+  "Format current date using unified date format."
+  []
+  (format-date (new java.util.Date)))
 
