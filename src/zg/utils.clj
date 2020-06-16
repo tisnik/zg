fb2ab9adfcd4294cdfb0ed4f842040f6f921b270
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-16 10:49:15 +0200

    Reformatted utils.clj by zprint

--- src/zg/utils.clj
+++ src/zg/utils.clj
@@ -11,47 +11,47 @@
 ;
 
 (ns zg.utils
-    "Various utility function used by other modules.")
+  "Various utility function used by other modules.")
 
 (defn third
-    "Simple utility function - returns third item from a given sequence, nil if third item does not exist."
-    [coll]
-    (nth coll 2 nil))
+  "Simple utility function - returns third item from a given sequence, nil if third item does not exist."
+  [coll]
+  (nth coll 2 nil))
 
 (defn fourth
-    "Simple utility function - returns fourth item from a given sequence, nil if third item does not exist."
-    [coll]
-    (nth coll 3 nil))
+  "Simple utility function - returns fourth item from a given sequence, nil if third item does not exist."
+  [coll]
+  (nth coll 3 nil))
 
 (defn substring
-    "Call method String.substring()."
-    ([^String s from to]
-     (.substring s from to))
-    ([^String s from]
-     (.substring s from)))
+  "Call method String.substring()."
+  ([^String s from to]
+    (.substring s from to))
+  ([^String s from]
+    (.substring s from)))
 
 (defn startsWith
-    "Call method String.startsWith()."
-    [^String s pattern]
-    (.startsWith s pattern))
+  "Call method String.startsWith()."
+  [^String s pattern]
+  (.startsWith s pattern))
 
 (defn endsWith
-    "Call method String.endsWith()."
-    [^String s pattern]
-    (.endsWith s pattern))
+  "Call method String.endsWith()."
+  [^String s pattern]
+  (.endsWith s pattern))
 
 (defn contains
-    "Call method String.contains()."
-    [^String s pattern]
-    (.contains s pattern))
+  "Call method String.contains()."
+  [^String s pattern]
+  (.contains s pattern))
 
 (defn replaceAll
-    "Call method String.replaceAll()."
-    [^String s pattern replacement]
-    (.replaceAll s pattern replacement))
+  "Call method String.replaceAll()."
+  [^String s pattern replacement]
+  (.replaceAll s pattern replacement))
 
 (defn get-exception-message
-    "Retrieve a message from given exception."
-    [^java.lang.Exception exception]
-    (.getMessage exception))
+  "Retrieve a message from given exception."
+  [^java.lang.Exception exception]
+  (.getMessage exception))
 
