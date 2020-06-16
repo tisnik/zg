acecb739eee0033e76e167e7cb552927c26163bb
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-16 10:50:33 +0200

    Reformatted db_spec.clj by zprint

--- src/zg/db_spec.clj
+++ src/zg/db_spec.clj
@@ -11,12 +11,12 @@
 ;
 
 (ns zg.db-spec
-    "Namespace that contains configuration of all JDBC sources.")
+  "Namespace that contains configuration of all JDBC sources.")
 
 (def zg-db
-    "Specification for SQLite database used for storing all dictionaries."
-    {:classname   "org.sqlite.JDBC"
-     :subprotocol "sqlite"
-     :subname     "zg.db"
-    })
+  "Specification for SQLite database used for storing all dictionaries."
+  {:classname   "org.sqlite.JDBC"
+   :subprotocol "sqlite"
+   :subname     "zg.db"
+  })
 
