4dec755b6753036dc0454d7034a176cba98f95a0
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-26 08:45:28 +0200

    Reformatted dictionary_interface_test.clj by zprint

--- test/zg/dictionary_interface_test.clj
+++ test/zg/dictionary_interface_test.clj
@@ -19,89 +19,91 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for various functions existence
 ;
 
 (deftest test-proper-word-for-whitelist?-existence
-    "Check that the zg.dictionary-interface/proper-word-for-whitelist? definition exists."
-    (testing "if the zg.dictionary-interface/proper-word-for-whitelist? definition exists."
-        (is (callable? 'zg.dictionary-interface/proper-word-for-whitelist?))))
+  "Check that the zg.dictionary-interface/proper-word-for-whitelist? definition exists."
+  (testing
+    "if the zg.dictionary-interface/proper-word-for-whitelist? definition exists."
+    (is (callable? 'zg.dictionary-interface/proper-word-for-whitelist?))))
 
 
 (deftest test-proper-word-for-blacklist?-existence
-    "Check that the zg.dictionary-interface/proper-word-for-blacklist? definition exists."
-    (testing "if the zg.dictionary-interface/proper-word-for-blacklist? definition exists."
-        (is (callable? 'zg.dictionary-interface/proper-word-for-blacklist?))))
+  "Check that the zg.dictionary-interface/proper-word-for-blacklist? definition exists."
+  (testing
+    "if the zg.dictionary-interface/proper-word-for-blacklist? definition exists."
+    (is (callable? 'zg.dictionary-interface/proper-word-for-blacklist?))))
 
 
 (deftest test-store-word-existence
-    "Check that the zg.dictionary-interface/store-word definition exists."
-    (testing "if the zg.dictionary-interface/store-word definition exists."
-        (is (callable? 'zg.dictionary-interface/store-word))))
+  "Check that the zg.dictionary-interface/store-word definition exists."
+  (testing "if the zg.dictionary-interface/store-word definition exists."
+           (is (callable? 'zg.dictionary-interface/store-word))))
 
 
 (deftest test-store-words-existence
-    "Check that the zg.dictionary-interface/store-words definition exists."
-    (testing "if the zg.dictionary-interface/store-words definition exists."
-        (is (callable? 'zg.dictionary-interface/store-words))))
+  "Check that the zg.dictionary-interface/store-words definition exists."
+  (testing "if the zg.dictionary-interface/store-words definition exists."
+           (is (callable? 'zg.dictionary-interface/store-words))))
 
 
 (deftest test-proper-word-for-blacklist?
-    "Check the function zg.dictionary-interface/proper-word-for-blacklist?"
-    (testing "the function zg.dictionary-interface/proper-word-for-blacklist?"
-        (are [x] (seq (proper-word-for-blacklist? x))
-            "test"
-            "Test"
-            "TEST"
-            "Test0"
-            "0"
-            "I'm"
-            "This is test"
-            "This is test."
-            "/"
-            "a/b"
-            "foo/bar")))
+  "Check the function zg.dictionary-interface/proper-word-for-blacklist?"
+  (testing "the function zg.dictionary-interface/proper-word-for-blacklist?"
+           (are [x] (seq (proper-word-for-blacklist? x))
+                "test"
+                "Test"
+                "TEST"
+                "Test0"
+                "0"
+                "I'm"
+                "This is test"
+                "This is test."
+                "/"
+                "a/b"
+                "foo/bar")))
 
 (deftest test-proper-word-for-blacklist?-negative
-    "Check the function zg.dictionary-interface/proper-word-for-blacklist?"
-    (testing "the function zg.dictionary-interface/proper-word-for-blacklist?"
-        (are [x] (not (seq (proper-word-for-blacklist? x)))
-            ""
-            "!"
-            "!!"
-            "! !"
-            "Test!"
-            "This is test!")))
+  "Check the function zg.dictionary-interface/proper-word-for-blacklist?"
+  (testing "the function zg.dictionary-interface/proper-word-for-blacklist?"
+           (are [x] (not (seq (proper-word-for-blacklist? x)))
+                ""
+                "!"
+                "!!"
+                "! !"
+                "Test!"
+                "This is test!")))
 
 (deftest test-proper-word-for-whitelist?
-    "Check the function zg.dictionary-interface/proper-word-for-whitelist?"
-    (testing "the function zg.dictionary-interface/proper-word-for-whitelist?"
-        (are [x] (seq (proper-word-for-whitelist? x))
-            "test"
-            "Test"
-            "TEST"
-            "Test0"
-            "0"
-            "I'm"
-            "This is test"
-            "This is test.")))
+  "Check the function zg.dictionary-interface/proper-word-for-whitelist?"
+  (testing "the function zg.dictionary-interface/proper-word-for-whitelist?"
+           (are [x] (seq (proper-word-for-whitelist? x))
+                "test"
+                "Test"
+                "TEST"
+                "Test0"
+                "0"
+                "I'm"
+                "This is test"
+                "This is test.")))
 
 (deftest test-proper-word-for-whitelist?-negative
-    "Check the function zg.dictionary-interface/proper-word-for-whitelist?"
-    (testing "the function zg.dictionary-interface/proper-word-for-whitelist?"
-        (are [x] (not (seq (proper-word-for-whitelist? x)))
-            ""
-            "!"
-            "!!"
-            "! !"
-            "Test!"
-            "This is test!"
-            "/"
-            "a/b"
-            "foo/bar")))
+  "Check the function zg.dictionary-interface/proper-word-for-whitelist?"
+  (testing "the function zg.dictionary-interface/proper-word-for-whitelist?"
+           (are [x] (not (seq (proper-word-for-whitelist? x)))
+                ""
+                "!"
+                "!!"
+                "! !"
+                "Test!"
+                "This is test!"
+                "/"
+                "a/b"
+                "foo/bar")))
 
