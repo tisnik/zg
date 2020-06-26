e806cda65fe64b25f5e49fb8184c1d305be98753
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-26 08:45:55 +0200

    Reformatted format_date_test.clj by zprint

--- test/zg/format_date_test.clj
+++ test/zg/format_date_test.clj
@@ -19,51 +19,51 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for various functions existence
 ;
 
 (deftest test-format-date-existence
-    "Check that the zg.format-date/format-date definition exists."
-    (testing "if the zg.format-date/format-date definition exists."
-        (is (callable? 'zg.format-date/format-date))))
+  "Check that the zg.format-date/format-date definition exists."
+  (testing "if the zg.format-date/format-date definition exists."
+           (is (callable? 'zg.format-date/format-date))))
 
 
 (deftest test-format-current-date-existence
-    "Check that the zg.format-date/format-current-date definition exists."
-    (testing "if the zg.format-date/format-current-date definition exists."
-        (is (callable? 'zg.format-date/format-current-date))))
+  "Check that the zg.format-date/format-current-date definition exists."
+  (testing "if the zg.format-date/format-current-date definition exists."
+           (is (callable? 'zg.format-date/format-current-date))))
 
 ;
 ; Test function behaviours
 ;
 
 (deftest test-format-date-1
-    "Check the function zg.format-date/format-date"
-    (testing "the function zg.format-date/format-date." 
-        (let [date (new java.util.Date 116 01 01 12 00)]
-            (is (not (nil? (format-date date))))
-            (is (= (type (format-date date)) java.lang.String))
-            (is (= (count (format-date date)) 19)))))
+  "Check the function zg.format-date/format-date"
+  (testing "the function zg.format-date/format-date."
+           (let [date (new java.util.Date 116 01 01 12 00)]
+             (is (not (nil? (format-date date))))
+             (is (= (type (format-date date)) java.lang.String))
+             (is (= (count (format-date date)) 19)))))
 
 (deftest test-format-date-2
-    "Check the function zg.format-date/format-date"
-    (testing "the function zg.format-date/format-date." 
-        (let [date (new java.util.Date 116 01 01 12 00)]
-            (is (= "2016-02-01 12:00:00" (format-date date))))
-        (let [date (new java.util.Date 100 01 01 12 00)]
-            (is (= "2000-02-01 12:00:00" (format-date date))))
-        (let [date (new java.util.Date 0 00 01 00 00)]
-            (is (= "1900-01-01 00:00:00" (format-date date))))))
+  "Check the function zg.format-date/format-date"
+  (testing "the function zg.format-date/format-date."
+           (let [date (new java.util.Date 116 01 01 12 00)]
+             (is (= "2016-02-01 12:00:00" (format-date date))))
+           (let [date (new java.util.Date 100 01 01 12 00)]
+             (is (= "2000-02-01 12:00:00" (format-date date))))
+           (let [date (new java.util.Date 0 00 01 00 00)]
+             (is (= "1900-01-01 00:00:00" (format-date date))))))
 
 (deftest test-format-current-date
-    "Check the function zg.format-date/format-current-date"
-    (testing "the function zg.format-date/format-current-date." 
-        (is (not (nil? (format-current-date))))
-        (is (= (type (format-current-date))))
-        (is (= (count (format-current-date)) 19))))
+  "Check the function zg.format-date/format-current-date"
+  (testing "the function zg.format-date/format-current-date."
+           (is (not (nil? (format-current-date))))
+           (is (= (type (format-current-date))))
+           (is (= (count (format-current-date)) 19))))
 
