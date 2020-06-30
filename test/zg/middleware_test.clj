9f0ce8879d57e4fff2cb0e9ada2aaa8a1de7687f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-30 08:49:06 +0200

    Reformatted middleware_test.clj by zprint

--- test/zg/middleware_test.clj
+++ test/zg/middleware_test.clj
@@ -19,48 +19,48 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for various functions existence
 ;
 
 (deftest test-inject-configuration-existence
-    "Check that the zg.middleware/inject-configuration definition exists."
-    (testing "if the zg.middleware/inject-configuration definition exists."
-        (is (callable? 'zg.middleware/inject-configuration))))
+  "Check that the zg.middleware/inject-configuration definition exists."
+  (testing "if the zg.middleware/inject-configuration definition exists."
+           (is (callable? 'zg.middleware/inject-configuration))))
 
 ;
 ; Tests for function behaviours.
 ;
 
 (deftest test-inject-confuguration-1
-    "Check the behaviour of function zg.middleware/inject-configuration."
-    (testing "The function zg.middleware/inject-configuration."
-        (let [function (inject-configuration (fn [x] x) :cfg)]
-            (are [x y] (= x y)
-                {:configuration :cfg} (function nil)
-                {:configuration :cfg} (function {})
-                {:configuration :cfg :foo :bar} (function {:foo :bar})))))
+  "Check the behaviour of function zg.middleware/inject-configuration."
+  (testing "The function zg.middleware/inject-configuration."
+           (let [function (inject-configuration (fn [x] x) :cfg)]
+             (are [x y] (= x y)
+                  {:configuration :cfg}            (function nil)
+                  {:configuration :cfg}            (function {})
+                  {:configuration :cfg, :foo :bar} (function {:foo :bar})))))
 
 (deftest test-inject-confuguration-2
-    "Check the behaviour of function zg.middleware/inject-configuration."
-    (testing "The function zg.middleware/inject-configuration."
-        (let [function (inject-configuration (fn [x] nil) :cfg)]
-            (are [x y] (= x y)
-                nil (function nil)
-                nil (function {})
-                nil (function {:foo :bar})
-                nil (function {:foo :bar :baz :bar})))))
+  "Check the behaviour of function zg.middleware/inject-configuration."
+  (testing "The function zg.middleware/inject-configuration."
+           (let [function (inject-configuration (fn [x] nil) :cfg)]
+             (are [x y] (= x y)
+                  nil (function nil)
+                  nil (function {})
+                  nil (function {:foo :bar})
+                  nil (function {:foo :bar, :baz :bar})))))
 
 (deftest test-inject-confuguration-not-NPE
-    "Check the behaviour of function zg.middleware/inject-configuration."
-    (testing "The function zg.middleware/inject-configuration."
-        (let [function (inject-configuration (fn [x] x) nil)]
-            (are [x y] (= x y)
-                {:configuration nil} (function nil)
-                {:configuration nil} (function {})
-                {:configuration nil :foo :bar} (function {:foo :bar})))))
+  "Check the behaviour of function zg.middleware/inject-configuration."
+  (testing "The function zg.middleware/inject-configuration."
+           (let [function (inject-configuration (fn [x] x) nil)]
+             (are [x y] (= x y)
+                  {:configuration nil}            (function nil)
+                  {:configuration nil}            (function {})
+                  {:configuration nil, :foo :bar} (function {:foo :bar})))))
 
